b93cd7ebf0ae4eae4363b2e44c7adfd1a026be02
yethoser81-hash <yethoser81@gmail.com>
2026-09-05 09:16:28 +0200

    nettoyage erreur

--- server.js
+++ server.js
@@ -246,7 +246,7 @@ app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use((req, res, next) => {
     res.setHeader(
         "Content-Security-Policy",
-        "default-src 'self' data: blob: https:; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://unpkg.com; style-src 'self' 'unsafe-inline' https://unpkg.com; img-src 'self' data: blob: https:;"
+        "default-src 'self' data: blob: https:; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://unpkg.com; style-src 'self' 'unsafe-inline' https://unpkg.com https://cdnjs.cloudflare.com; img-src 'self' data: blob: https:;"
     );
     res.setHeader("X-Content-Type-Options", "nosniff");
     res.setHeader("X-Frame-Options", "DENY");
